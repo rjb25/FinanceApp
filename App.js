80e2cf0b17771458f28a712ff4a568810c33e905
rjb25 <jason.basanese@gmail.com>
2021-09-28 19:49:56 -0400

    Zach is a little girl commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Brandon is a little girl</Text>
+
+      <Button 
+title="Click Me" 
+	onPress={() => Alert.alert("Who is little girl", "So is Zach", [{text:"yes"},{text:"YES"}])}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
